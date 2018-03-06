81f3f505091e2b0e38aac0895d59fa252bfef0df
Tucker Platt <tucker@bendcable.com>
2018-03-06 21:25:16 +0000

    todo destroy complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,18 +13,18 @@ class ItemsController < ApplicationController
     end
   end
   
-  # def destroy
-  #   @item = current_user.items.find(params[:id])
-  #   @item.destroy
+  def destroy
+    @item = current_user.items.find(params[:id])
+    @item.destroy
     
-  #   redirect_to current_user
-  # end
-  
-  # if @item.destroy
-  #   flash[:notice] = "Item completed and removed successfully."
-  # else
-  #   flash[:error] = "There was an error removing the item. Please try again."
-  # end
+    redirect_to current_user
+ 
+    if @item.destroy
+      flash[:notice] = "Item completed and removed successfully."
+    else
+      flash[:error] = "There was an error removing the item. Please try again."
+    end
+  end
   
   private
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,4 @@ class UsersController < ApplicationController
     @user = current_user
   end
   
-  def destroy
-    @item = current_user.items.find(params[:id])
-    @item.destroy
-    
-    if @item.destroy
-      flash[:notice] = "Item completed and removed successfully."
-    else
-      flash[:error] = "There was an error removing the item. Please try again."
-    end
-    redirect_to current_user
-  end
 end
